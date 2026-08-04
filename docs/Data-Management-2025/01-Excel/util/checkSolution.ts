422cb62f94a4313006f0c6e0bfe2903552a273f9
Silas Berger <silas.berger@outlook.com>
2026-08-04 15:35:59 +0200

    Prepare archive for GYM28 and FMS28.

--- docs/Data-Management-2025/01-Excel/util/checkSolution.ts
+++
@@ -1,6 +0,0 @@
-export const checkExcelFormulaAnswer = (val: string, solutions: string[]) => {
-    if (!val) {
-        return false;
-    }
-    return solutions.some((solution: string) => solution === val);
-};
